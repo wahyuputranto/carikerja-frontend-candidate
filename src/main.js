2da9170752ea9a6a6d45e99a85c94e55008bb095
Putrantoz <34730965+wahyuputranto@users.noreply.github.com>
2025-11-23 15:16:37 +0700

    updated

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-import './style.css'
+import './tailwind.css';
+import './custom.css';
 
 const app = createApp(App)
 const pinia = createPinia()
