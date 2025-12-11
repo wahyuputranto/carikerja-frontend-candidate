169febd694cb9f4b6fe34d629b4878c5ded810ac
Putrantoz <34730965+wahyuputranto@users.noreply.github.com>
2025-12-11 23:45:05 +0700

    Add vite-plugin-html-env and update HTML metadata
    Introduced vite-plugin-html-env to enable environment variable usage in HTML files. Updated index.html to use environment variables for app name and description, and switched favicon links to external SVG assets for improved branding.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
